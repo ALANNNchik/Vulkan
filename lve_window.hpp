a18fdd1b21210bdf6d18c4d9a024e5217c9fe01a
Desolation <alanzhanat25@gmail.com>
2024-04-13 23:13:12 +0600

    Vulkan Reaktor
    Do not work!

---
+++ lve_window.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <string>
+
+namespace lve {
+
+	class LveWindow {
+	public:
+		LveWindow(int W, int h, std::string name);
+		~LveWindow();
+
+		LveWindow(const LveWindow&) = delete;
+		LveWindow& operator=(const LveWindow&) = delete;
+
+		bool shouldClose() { return glfwWindowShouldClose(window); }
+		VkExtent2D getExtent() { return { static_cast<uint32_t> (width), static_cast<uint32_t> (height) }; }
+
+		void createWindowSurface(VkInstance instance, VkSurfaceKHR* surface);
+
+	private:
+		void initWindow();
+
+		int width;
+		int height;
+
+		std::string windowName;
+		GLFWwindow* window;
+	};
+}
